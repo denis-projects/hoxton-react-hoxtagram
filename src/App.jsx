bbcf25300f22cf6f6414b921b534e1d0a470b026
besmaerteralb <denis.mimini.1@outlook.com>
2022-01-12 08:30:29 +0100

    adding some functions

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,36 @@ import Logo from './Components/Logo'
 import Posts from './Components/Posts'
 
 function App() {
-    const [posts, setPost] = useState([])
-
+    const [images, setImages] = useState([]);
     useEffect(() => {
-        fetch(`http://localhost:8000/images`).then(resp => resp.json())
-            .then(posts => setPost(posts))
-    }, [])
+        fetch("http://localhost:3000/images")
+            .then((resp) => resp.json())
+            .then((images) => setImages(images));
+        console.log(images);
+    }, []);
 
+    function showLikes(image) {
+        return fetch(`http://localhost:3000/images/${image.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(image),
+        }).then((resp) => resp.json());
+    }
 
+    function increaseLikes(image) {
+        const update = [...images];
+        image.likes++;
+        showLikes(image);
+        setImages(update);
+    }
 
     return (
         <div className="App">
             <Logo />
 
-            <Posts posts={posts} />
+            <Posts posts={Posts} />
 
         </div>
     )
